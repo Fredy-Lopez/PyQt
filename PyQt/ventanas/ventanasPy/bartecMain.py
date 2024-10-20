680c2bfe17eeb9a7eb2e0724e231b01decdd77dc
Fredy-Lopez <flopezm@fctunca.edu.py>
2024-10-20 20:15:03 -0300

    Update bartecMain.py

--- PyQt/ventanas/ventanasPy/bartecMain.py
+++ PyQt/ventanas/ventanasPy/bartecMain.py
@@ -1,7 +1,9 @@
 import sys
 from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QDialog
 from PyQt5.uic import loadUi
-from PyQt5.QtCore import Qt 
+from PyQt5.QtCore import Qt, QSize, QTimer, QIODevice
+from PyQt5.QtGui import QMovie
+from PyQt5.QtSerialPort import QSerialPort, QSerialPortInfo
 
 # Importaciones de los recursos
 sys.path.append(r'C:\Users\fredy\Desktop\Estudios\Programacion\Python\PyQt\Bebidas')
@@ -20,6 +22,9 @@ class MainApp(QMainWindow):
         super().__init__()
         loadUi("C:/Users/fredy/Desktop/Estudios/Programacion/Python/PyQt/ventanas/ventanaMenu.ui", self)  
         self.showFullScreen()
+
+        self.serial = QSerialPort()
+        self.serial.setBaudRate(9600)
         
         # Configurar el cursor de mano para los botones
         self.bJugo.setCursor(Qt.PointingHandCursor)  
@@ -38,10 +43,10 @@ class MainApp(QMainWindow):
         bebidasOpJugo = [
         ("PIÑA COOLER", "Jugo de piña, jugo de limón, agua tonica."),
         ("NARANJA SUNRISE", "Jugo de naranja, jugo de limón."),
-        ("LIMONADA", "Jugo de limón, agua tonica, azúcar."),
+        ("LIMONETTO", "Jugo de limón, agua tonica."),
         ("NARANJADA", "Jugo de naranja, agua tonica."),
         ("CITRUS PUNCH", "Jugo de limón, jugo de piña, jugo de naranja."),
-        ("LIMONADA ESPUMOSA", "Jugo de limón, Coca-Cola."),
+        ("CITRUS BURST", "Jugo de limón, jugo de pomelo."),
         ("PIÑA COLADA SIN ALCOHOL", "Jugo de piña, jugo de limón.")
         ]
         vent = "Jugo"
@@ -65,8 +70,8 @@ class MainApp(QMainWindow):
         self.ventana_jugo.jCitrusPunch.setCursor(Qt.PointingHandCursor)
         self.ventana_jugo.jCitrusPunch.clicked.connect(lambda: self.vConfirmar(bebidasOpJugo[4], vent))
 
-        self.ventana_jugo.jLimonadaEspumosa.setCursor(Qt.PointingHandCursor)
-        self.ventana_jugo.jLimonadaEspumosa.clicked.connect(lambda: self.vConfirmar(bebidasOpJugo[5], vent))
+        self.ventana_jugo.jCitrusBurst.setCursor(Qt.PointingHandCursor)
+        self.ventana_jugo.jCitrusBurst.clicked.connect(lambda: self.vConfirmar(bebidasOpJugo[5], vent))
 
         self.ventana_jugo.jPinaColada.setCursor(Qt.PointingHandCursor)
         self.ventana_jugo.jPinaColada.clicked.connect(lambda: self.vConfirmar(bebidasOpJugo[6], vent))
@@ -82,9 +87,9 @@ class MainApp(QMainWindow):
         ("DESTORNILLADOR", "Vodka, jugo de naranja."),
         ("VODKA TONIC", "Vodka, agua tónica."),
         ("CAIPIROSKA", "Vodka, jugo de limón."),
-        ("VODKA COLLINS", "Vodka, jugo de limón, agua tónica."),
+        ("SALTY DOG", "Vodka, jugo de pomelo."),
         ("CITRUS VODKA PUNCH", "Vodka, jugo de limón, jugo de naranja."),
-        ("VODKA LIMON COLA", "Vodka, Coca-Cola, jugo de limón.")
+        ("VODKA CITRUS BREEZE", "Vodka, jugo de pomelo, jugo de limón.")
         ]
         vent = "Vodka"
         self.ocultarVentana()  # Ocultar la ventana principal
@@ -101,14 +106,14 @@ class MainApp(QMainWindow):
         self.ventana_vodka.vCaipiroska.setCursor(Qt.PointingHandCursor)
         self.ventana_vodka.vCaipiroska.clicked.connect(lambda: self.vConfirmar(bebidasOpVodka[2], vent))
 
-        self.ventana_vodka.vCollins.setCursor(Qt.PointingHandCursor)
-        self.ventana_vodka.vCollins.clicked.connect(lambda: self.vConfirmar(bebidasOpVodka[3], vent))
+        self.ventana_vodka.vSaltyDog.setCursor(Qt.PointingHandCursor)
+        self.ventana_vodka.vSaltyDog.clicked.connect(lambda: self.vConfirmar(bebidasOpVodka[3], vent))
 
         self.ventana_vodka.vCitrusPunch.setCursor(Qt.PointingHandCursor)
         self.ventana_vodka.vCitrusPunch.clicked.connect(lambda: self.vConfirmar(bebidasOpVodka[4], vent))
 
-        self.ventana_vodka.vLimonCola.setCursor(Qt.PointingHandCursor)
-        self.ventana_vodka.vLimonCola.clicked.connect(lambda: self.vConfirmar(bebidasOpVodka[5], vent))
+        self.ventana_vodka.vCitrusBreeze.setCursor(Qt.PointingHandCursor)
+        self.ventana_vodka.vCitrusBreeze.clicked.connect(lambda: self.vConfirmar(bebidasOpVodka[5], vent))
 
 
         # Conectar el botón para volver a la ventanaMenu
@@ -119,13 +124,13 @@ class MainApp(QMainWindow):
 
     def AbrirVentanaCachaza(self):
         bebidasOpCachaza = [
-        ("Caipirinha", "Cachaza, jugo de limón."),
-        ("Caipifruta", "Cachaza, jugo de piña, jugo de limón, azúcar."),
-        ("Cachaza Tonic", "Cachaza, jugo de limón, agua tónica."),
-        ("Tropical Caipirinha", "Cachaza, jugo de piña, jugo de limón."),
-        ("Caipirinha de Coca", "Cachaza, jugo de limón, Coca-Cola."),
-        ("Batida Citrus", "Cachaza, jugo de naranja, jugo de limón."),
-        ("Cachaza Cooler", "Cachaza, jugo de limón, agua tónica, \njugo de naranja.")
+        ("CAIPIRINHA", "Cachaza, jugo de limón."),
+        ("CAIPIFRUTA", "Cachaza, jugo de piña, jugo de limón."),
+        ("CACHAZA TONIC", "Cachaza, jugo de pomelo, agua tónica."),
+        ("TROPICAL CAIPIRINHA", "Cachaza, jugo de piña, jugo de limón."),
+        ("CAIPIRINHA POMELO", "Cachaza, jugo de limón, jugo de pomelo."),
+        ("BATIDA CITRUS", "Cachaza, jugo de naranja, jugo de limón."),
+        ("CACHAZA COOLER", "Cachaza, jugo de limón, agua tónica, \njugo de naranja.")
         ]
         vent = "Cachaza"
         self.ocultarVentana()  # Ocultar la ventana principal
@@ -145,8 +150,8 @@ class MainApp(QMainWindow):
         self.ventana_cachaza.cTropicalCaipirinha.setCursor(Qt.PointingHandCursor)
         self.ventana_cachaza.cTropicalCaipirinha.clicked.connect(lambda: self.vConfirmar(bebidasOpCachaza[3], vent))
 
-        self.ventana_cachaza.cCaipirinhaCoca.setCursor(Qt.PointingHandCursor)
-        self.ventana_cachaza.cCaipirinhaCoca.clicked.connect(lambda: self.vConfirmar(bebidasOpCachaza[4], vent))
+        self.ventana_cachaza.cCaipirinhaPomelo.setCursor(Qt.PointingHandCursor)
+        self.ventana_cachaza.cCaipirinhaPomelo.clicked.connect(lambda: self.vConfirmar(bebidasOpCachaza[4], vent))
 
         self.ventana_cachaza.cBatidaCitrus.setCursor(Qt.PointingHandCursor)
         self.ventana_cachaza.cBatidaCitrus.clicked.connect(lambda: self.vConfirmar(bebidasOpCachaza[5], vent))
@@ -162,12 +167,12 @@ class MainApp(QMainWindow):
 
     def AbrirVentanaRon(self):
         bebidasOpRon = [
-        ("Piña Colada", "Ron, jugo de piña."),
-        ("Cuba Libre", "Ron, Coca-Cola, jugo de limón."),
-        ("Tropical Punch", "Ron, jugo de piña, jugo de limón."),
-        ("Ron Punch", "Ron, jugo de piña, jugo de limón, jugo de naranja."),
-        ("Ron Tonic Tropical", "Ron, agua tónica, jugo de piña, jugo de limón."),
-        ("Tropical Cuba Libre", "Ron, Coca-Cola, jugo de piña, jugo de limón.")
+        ("PIÑA COLADA", "Ron, jugo de piña."),
+        ("POMELO RUM SPLASH", "Ron, jugo de pomelo, jugo de limón."),
+        ("TROPICAL PUNCH", "Ron, jugo de piña, jugo de limón."),
+        ("RON PUNCH", "Ron, jugo de piña, jugo de limón, jugo de naranja."),
+        ("RON TONIC TROPICAL", "Ron, agua tónica, jugo de piña, jugo de limón."),
+        ("TROPICAL RUM STORM", "Ron, jugo de pomelo, jugo de piña, jugo de limón.")
         ]
         vent = "Ron"
         self.ocultarVentana()  # Ocultar la ventana principal
@@ -178,8 +183,8 @@ class MainApp(QMainWindow):
         self.ventana_ron.rPinaColada.setCursor(Qt.PointingHandCursor)
         self.ventana_ron.rPinaColada.clicked.connect(lambda: self.vConfirmar(bebidasOpRon[0], vent))
 
-        self.ventana_ron.rCubaLibre.setCursor(Qt.PointingHandCursor)
-        self.ventana_ron.rCubaLibre.clicked.connect(lambda: self.vConfirmar(bebidasOpRon[1], vent))
+        self.ventana_ron.rPomeloRumSplash.setCursor(Qt.PointingHandCursor)
+        self.ventana_ron.rPomeloRumSplash.clicked.connect(lambda: self.vConfirmar(bebidasOpRon[1], vent))
 
         self.ventana_ron.rTropicalPunch.setCursor(Qt.PointingHandCursor)
         self.ventana_ron.rTropicalPunch.clicked.connect(lambda: self.vConfirmar(bebidasOpRon[2], vent))
@@ -190,8 +195,8 @@ class MainApp(QMainWindow):
         self.ventana_ron.rRonTonic.setCursor(Qt.PointingHandCursor)
         self.ventana_ron.rRonTonic.clicked.connect(lambda: self.vConfirmar(bebidasOpRon[4], vent))
 
-        self.ventana_ron.rTropicalCubaLibre.setCursor(Qt.PointingHandCursor)
-        self.ventana_ron.rTropicalCubaLibre.clicked.connect(lambda: self.vConfirmar(bebidasOpRon[5], vent))
+        self.ventana_ron.rTropicalRumStorm.setCursor(Qt.PointingHandCursor)
+        self.ventana_ron.rTropicalRumStorm.clicked.connect(lambda: self.vConfirmar(bebidasOpRon[5], vent))
 
         # Conectar el botón para volver a la ventanaMenu
         self.ventana_ron.rBoton.setCursor(Qt.PointingHandCursor)
@@ -213,6 +218,8 @@ class MainApp(QMainWindow):
             self.ventana_cachaza.close()
         if hasattr(self, 'ventana_ron'):
             self.ventana_ron.close()
+        if hasattr(self, 'ventana_listo'):
+            self.ventana_listo.close()
 
     def vConfirmar(self, bebidasOp, vent):
         Bebida, Contenido = bebidasOp
@@ -229,20 +236,24 @@ class MainApp(QMainWindow):
         self.Confirmar.jBebida.setText(f"Seleccionaste: {Bebida}")
         self.Confirmar.jContenido.setText(f"Contiene: {Contenido}")
 
+        self.Confirmar.jCambiar.setCursor(Qt.PointingHandCursor)
+        self.Confirmar.jConfirmar.setCursor(Qt.PointingHandCursor)
         self.Confirmar.jCambiar.clicked.connect(self.Confirmar.close)
         if tipVent == "Jugo":
-            self.Confirmar.jConfirmar.clicked.connect(self.vEspera)
+            self.Confirmar.jConfirmar.clicked.connect(self.Confirmar.close)
+            self.Confirmar.jConfirmar.clicked.connect(lambda: self.vEspera(Bebida, tipVent))
         elif tipVent == "Vodka" or "Cachaza" or "Ron":
             self.Confirmar.jConfirmar.setText("CONFIRMAR Y CONTINUAR")
             self.Confirmar.jConfirmar.clicked.connect(self.Confirmar.close)
-            self.Confirmar.jConfirmar.clicked.connect(lambda: self.vIntensidad(Bebida))
-
+            self.Confirmar.jConfirmar.clicked.connect(lambda: self.vIntensidad(Bebida, tipVent))
 
         # Mostrar la ventana de confirmación
         self.Confirmar.exec_()
 
-    def vIntensidad(self, Bebida):
+    def vIntensidad(self, Bebida, tipVent):
         Bebida = Bebida
+        tipVent = tipVent
+        self.intensidad = ""
         self.Intensidad = QDialog()
         loadUi("C:/Users/fredy/Desktop/Estudios/Programacion/Python/PyQt/ventanas/vIntensidad.ui", self.Intensidad)
         # Configurar propiedades de la ventana de confirmación
@@ -252,20 +263,129 @@ class MainApp(QMainWindow):
         self.Intensidad.setGeometry(550, 270, 750, 600)
         
         self.Intensidad.Bebida.setText(f"Seleccionaste: {Bebida}")
-
+        self.Intensidad.Cambiar.setCursor(Qt.PointingHandCursor)
         self.Intensidad.Cambiar.clicked.connect(self.Intensidad.close)
+
+        self.Intensidad.Confirmar.setEnabled(False)
+
+        self.Intensidad.Suave.toggled.connect(self.HabilitarBotonConfirmar)
+        self.Intensidad.Normal.toggled.connect(self.HabilitarBotonConfirmar)
+        self.Intensidad.Fuerte.toggled.connect(self.HabilitarBotonConfirmar)
+
+        self.Intensidad.Confirmar.clicked.connect(self.Intensidad.close)
+        self.Intensidad.Confirmar.clicked.connect(lambda: self.vEspera(Bebida, tipVent))
 
         # Mostrar la ventana de confirmación
         self.Intensidad.exec_()
 
+    def HabilitarBotonConfirmar(self):
+        # Habilitar el botón de confirmar si al menos un radio button está seleccionado
+        self.Intensidad.Confirmar.setEnabled(self.Intensidad.Suave.isChecked() or self.Intensidad.Normal.isChecked() or self.Intensidad.Fuerte.isChecked())
+        self.Intensidad.Confirmar.setCursor(Qt.PointingHandCursor)
+        if self.Intensidad.Suave.isChecked():
+            self.intensidad = " SUAVE"
+        if self.Intensidad.Normal.isChecked():
+            self.intensidad = " NORMAL"    
+        if self.Intensidad.Fuerte.isChecked():
+            self.intensidad = " FUERTE"    
+        print(f"intensidad: {self.intensidad}")
 
-    def vEspera(self):
-        pass
+    def vEspera(self, Bebida, tipVent):
+        Bebida = Bebida
+        tipVent = tipVent
+        if tipVent == "Vodka":
+            self.ventana_vodka.close()
+        elif tipVent == "Cachaza":
+            self.ventana_cachaza.close()
+        elif tipVent == "Ron":
+            self.ventana_ron.close()
+        elif tipVent == "Jugo":
+            self.ventana_jugo.close()
+
+        self.detectarPuerto(Bebida)
+        
+        if self.serial.isOpen():
+            if tipVent == "Jugo":
+                bebida_bytes = Bebida.encode('utf-8')  # Convertir el mensaje a bytes
+                self.serial.write(bebida_bytes)
+                print(f"Mensaje enviado al Arduino: {Bebida}") 
+            elif tipVent == "Cachaza" or tipVent == "Ron" or tipVent == "Vodka" :
+                bebida_bytes = Bebida.encode('utf-8')  # Convertir el mensaje a bytes
+                intensidad_bytes = self.intensidad.encode('utf-8')
+                self.serial.write(bebida_bytes)
+                print(f"Mensaje enviado al Arduino: {Bebida}")
+                self.serial.write(intensidad_bytes)
+                print(f"Intensidad de la bebida: {self.intensidad}")
+        else:
+            print("El puerto serial no está abierto.")
+        self.ventana_espera = QMainWindow()
+        loadUi("C:/Users/fredy/Desktop/Estudios/Programacion/Python/PyQt/ventanas/vEspera.ui", self.ventana_espera)
+        # Cargar el GIF usando barras inclinadas
+        self.movie = QMovie("C:/Users/fredy/Desktop/Estudios/Programacion/Python/PyQt/Bebidas/Gifs/cargar.gif")
+        self.ventana_espera.gifCargar.setMovie(self.movie)
+        self.movie.start()  # Iniciar la animación del GIF
+        self.movie.setScaledSize(QSize(115, 115))
+        self.ventana_espera.showFullScreen()
+
+    def detectarPuerto(self, Bebida):
+        Bebida = Bebida
+        # Obtener la lista de puertos seriales disponibles
+        available_ports = QSerialPortInfo.availablePorts()
+        
+        for port in available_ports:
+            # Puedes imprimir la información del puerto para depurar
+            print(f"Puerto: {port.portName()}, Descripción: {port.description()}, Fabricante: {port.manufacturer()}")
+
+            # Intentar identificar el puerto correcto basándote en la descripción o fabricante
+            if "Arduino" in port.description() or "Arduino" in port.manufacturer():
+                # Si encuentras el puerto, intenta abrirlo
+                self.serial = QSerialPort(port)
+                self.serial.setBaudRate(9600)
+                
+                if self.serial.open(QIODevice.ReadWrite):
+                    print(f"Conectado al puerto: {port.portName()}")
+                    # Conectar la señal de lectura serial
+                    self.serial.readyRead.connect(lambda: self.serialRecibido(Bebida))
+                    return
+                else:
+                    print(f"No se pudo abrir el puerto: {port.portName()}")
+        
+        # Si no se encuentra el puerto Arduino
+        print("No se encontró un Arduino conectado.")
+    
+    def serialRecibido(self, Bebida):
+        Bebida = Bebida
+        # Leer los datos del puerto serial
+        if self.serial.canReadLine():
+            data = self.serial.readLine().data().decode().strip()
+            print(f"Datos recibidos del puerto serial: {data}") 
+            # Verificar si el dato recibido es el que estamos esperando
+            if data == "LISTO":  # Ajustar según el mensaje enviado por el Arduino
+                self.vListo(Bebida)
+
+    
+    def vListo(self, Bebida):
+        Bebida = Bebida
+        self.ventana_espera.close()
+        self.ventana_listo = QMainWindow()
+        loadUi("C:/Users/fredy/Desktop/Estudios/Programacion/Python/PyQt/ventanas/vListo.ui", self.ventana_listo)
+        # Cargar el GIF usando barras inclinadas
+        self.movieFin = QMovie("C:/Users/fredy/Desktop/Estudios/Programacion/Python/PyQt/Bebidas/Gifs/bebidaFinalizar.gif")
+        self.ventana_listo.gifbebidaFin.setMovie(self.movieFin)
+        self.movieFin.start()  # Iniciar la animación del GIF
+        self.movieFin.setScaledSize(QSize(350, 350))
+
+        self.ventana_listo.gracias.setText(f"Por favor, recoge tu {Bebida}\nGRACIAS POR SU COMPRA")
+        self.ventana_listo.btnFin.clicked.connect(self.volverAMenu)
+        self.ventana_listo.btnFin.setCursor(Qt.PointingHandCursor)
+        self.ventana_listo.showFullScreen()
+        
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     main_window = MainApp()
     main_window.show()  # Mostrar la ventana principal
     sys.exit(app.exec_())
+
 
 
